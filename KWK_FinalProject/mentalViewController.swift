2fef94ca0d142067213d7fdf6c2a3a103a6030df
Rebecca <rwfairbanks1@gmail.com>
2021-07-22 09:46:16 -0500

    code

---
+++ KWK_FinalProject/mentalViewController.swift
@@ -0,0 +1,43 @@
+//
+//  mentalViewController.swift
+//  KWK_FinalProject
+//
+//  Created by  Scholar on 7/22/21.
+//
+
+import UIKit
+
+class mentalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func stronglyagree1(_ sender: Any) {
+    }
+    
+    @IBAction func agree1(_ sender: Any) {
+    }
+    
+     @IBAction func stronglydisagree1(_ sender: UIButton) {
+     }
+    
+    
+     @IBAction func stronglyagree2(_ sender: UIButton) {
+     }
+    
+    
+    
+    /*
+     // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
